c93338b66202008e5e0b0304bd3277513c146cc1
Wendy Smoak <wsmoak@gmail.com>
2016-03-08 08:59:00 -0500

    First pass at simple StartApp program

--- web/elm/MyColors.elm
+++ web/elm/MyColors.elm
@@ -1,6 +1,35 @@
 module MyColors where
 
+import StartApp
 import Html
+import Effects exposing (Effects, Never)
+import Task exposing ( Task )
 
+app =
+    StartApp.start { init = init, view = view, update = update, inputs = [] }
+
+main : Signal Html.Html
 main =
-  Html.text "Hello from MyColors in Elm!"
+    app.html
+
+view : Signal.Address action -> model -> Html.Html
+view address model =
+  Html.text "Hello from Elm StartApp!"
+
+type alias Model = String
+
+inputs : List (Signal action)
+inputs = []
+
+type Action = NoOp
+
+update : action -> model -> (model, Effects action)
+update action model =
+  ( model, Effects.none )
+
+init : (Model, Effects Action)
+init = ("", Effects.none)
+
+port tasks : Signal (Task.Task Never ())
+port tasks =
+    app.tasks
